de6eadcd7e7339432dcc8c621c137b889424ecda
Dmytro <stavok22@gmail.com>
2023-03-02 13:35:37 +0200

    master

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,7 +2,6 @@ package com.mycompany.app;
 
 
 import com.google.gson.GsonBuilder;
-
 public class App {
     public static void main( String[] args ) {
         User user = new User("Dmytro","Stavyshchenko");
